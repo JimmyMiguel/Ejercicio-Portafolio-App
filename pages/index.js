c0f578775af747021b3146e06e48f49630d5a534
Jimmy <jimmymiguelgalarza@gmail.com>
2025-08-10 18:09:47 -0500

    Terminado , falta el formulario

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,29 @@
-function main(){
-    
-    createHeaderCom("headerCom");
-    createCards("workPlace");
+function main() {
+  createHeaderCom("headerCom");
+  createCards("workPlace");
+
+  fetch(
+    "https://preview.contentful.com/spaces/ijty4i0htq9n/environments/master/assets/1Q1DRQV3Afo70Pz874ScO9?access_token=_k3riHq77wPayIXr_svPPX7Cgpy3Tas0qQQu8wn-Kuw"
+  ).then((respuesta) => {
+    if (!respuesta.ok) {
+      throw new Error(`Error HTTP: ${response.status}`);
+    }
+
+    return respuesta.json()
+  })
+  .then(
+    (data)=>{
+        console.log(data)
+        const imagen  = document.querySelector(".abautMe img")
+        imagen.src = data.fields.file.url
+
+
+
+    }
+  )
 
 
 
 }
 
-main()
+main();
